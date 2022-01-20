154b69d1e7c2a5b0fe27bef1001c00266eb017de
Apos-dot-net <kumaradityasr@gmail.com>
2022-01-20 15:58:14 +0530

    First Commit

---
+++ cmake-build-debug/CMakeFiles/DSC_Mini_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSC_Mini_Project.dir/main.c.obj"
+  "DSC_Mini_Project.exe"
+  "DSC_Mini_Project.exe.manifest"
+  "DSC_Mini_Project.pdb"
+  "libDSC_Mini_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DSC_Mini_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DSC_Mini_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DSC_Mini_Project.
+# This may be replaced when dependencies are built.
